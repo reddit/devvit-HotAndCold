24fe562650da649168930e90a24ccf741c8276fc
Thomas Wang <thomas.wang@reddit.com>
2025-04-29 11:52:51 -0700

    pr feedback: extract page content to share with modal

---
+++ packages/classic-webview/src/components/UnlockHardcoreCTAContent.tsx
@@ -0,0 +1,30 @@
+import { PurchaseButton } from './PurchaseButton';
+import { HardcoreLogo } from '@hotandcold/webview-common/components/logo';
+
+type UnlockHardcoreCTAContentProps = {
+  withLogo?: boolean;
+};
+export const UnlockHardcoreCTAContent = ({ withLogo }: UnlockHardcoreCTAContentProps) => {
+  return (
+    <div className="flex w-full max-w-md flex-col items-center justify-center gap-4 p-6 sm:gap-6 sm:p-8 md:max-w-2xl md:p-10">
+      {withLogo && <HardcoreLogo />}
+      <p className="text-xl font-bold text-white sm:text-2xl">100 guesses. No hints. No mercy.</p>
+      <p className="text-sm font-normal text-gray-300">
+        Unlocking Hardcore grants access to today and all previous hardcore puzzles.
+      </p>
+      <hr className="h-px w-1/2 max-w-xs bg-white/20"></hr>
+      <div className="flex w-full flex-col items-center gap-4 py-2 sm:w-auto sm:flex-row sm:items-start">
+        <PurchaseButton price={50} style="secondary" productSku="hardcore-mode-seven-day-access">
+          Unlock for 7 days
+        </PurchaseButton>
+        <PurchaseButton price={250} style="primary" productSku="hardcore-mode-lifetime-access">
+          Unlock FOREVER
+        </PurchaseButton>
+      </div>
+      <p className="text-center text-xs font-normal text-slate-400">
+        Looking for today's puzzle?{' '}
+        <span className="underline decoration-solid underline-offset-auto">Click here</span>
+      </p>
+    </div>
+  );
+};
--- packages/classic-webview/src/pages/PlayPage.tsx
+++ packages/classic-webview/src/pages/PlayPage.tsx
@@ -12,7 +12,7 @@ import { PageContentContainer } from '../components/pageContentContainer';
 import { cn } from '@hotandcold/webview-common/utils';
 import { HARDCORE_MAX_GUESSES } from '../constants';
 import { useHardcoreAccess } from '../hooks/useHardcoreAccess';
-import { PurchaseButton } from '../components/PurchaseButton';
+import { UnlockHardcoreCTAContent } from '../components/UnlockHardcoreCTAContent';
 
 const useFeedback = (): { feedback: FeedbackResponse | null; dismissFeedback: () => void } => {
   const [feedback, setFeedback] = useState<FeedbackResponse | null>(null);
@@ -116,7 +116,7 @@ export const PlayPage = () => {
   let content;
 
   if (isHardcore && access.status !== 'active') {
-    content = <UnlockHardcorePageContent />;
+    content = <UnlockHardcoreCTAContent />;
   } else {
     content = <GameplayContent />;
   }
@@ -128,30 +128,6 @@ export const PlayPage = () => {
     >
       {content}
     </PageContentContainer>
-  );
-};
-
-const UnlockHardcorePageContent = () => {
-  return (
-    <div className="flex w-full max-w-md flex-col items-center justify-center gap-4 p-6 sm:gap-6 sm:p-8 md:max-w-2xl md:p-10">
-      <p className="text-xl font-bold text-white sm:text-2xl">100 guesses. No hints. No mercy.</p>
-      <p className="text-sm font-normal text-gray-300">
-        Unlocking Hardcore grants access to today and all previous hardcore puzzles.
-      </p>
-      <hr className="h-px w-1/2 max-w-xs bg-white/20"></hr>
-      <div className="flex w-full flex-col items-center gap-4 py-2 sm:w-auto sm:flex-row sm:items-start">
-        <PurchaseButton price={50} style="secondary" productSku="hardcore-mode-seven-day-access">
-          Unlock for 7 days
-        </PurchaseButton>
-        <PurchaseButton price={250} style="primary" productSku="hardcore-mode-lifetime-access">
-          Unlock FOREVER
-        </PurchaseButton>
-      </div>
-      <p className="text-slate-400 text-center text-[12px] font-normal">
-        Looking for today's puzzle?{' '}
-        <span className="underline decoration-solid underline-offset-auto">Click here</span>
-      </p>
-    </div>
   );
 };
 
