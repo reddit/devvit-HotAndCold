9ae7d0389a26599da89a8b5a8a21f3cdc10da2e8
Diego Hernandez <diego.hernandez@reddit.com>
2025-04-30 13:14:10 -0700

    Hardcore mode copy changes

--- packages/classic-webview/src/components/UnlockHardcoreCTAContent.tsx
+++ packages/classic-webview/src/components/UnlockHardcoreCTAContent.tsx
@@ -13,9 +13,7 @@ export const UnlockHardcoreCTAContent = ({
     <div className="flex w-full max-w-md flex-col items-center justify-center gap-4 p-6 sm:gap-6 sm:p-8 md:max-w-2xl md:p-10">
       {withLogo && <HardcoreLogo />}
       <p className="text-xl font-bold text-white sm:text-2xl">100 guesses. No hints. No mercy.</p>
-      <p className="text-sm font-normal text-gray-300">
-        Unlocking Hardcore grants access to today and all previous hardcore puzzles.
-      </p>
+      <p className="text-sm font-normal text-gray-300">Get access to expert-level puzzles</p>
       <hr className="h-px w-1/2 max-w-xs bg-white/20"></hr>
       <div className="flex w-full flex-col items-center gap-4 py-2 sm:w-auto sm:flex-row sm:items-start">
         <PurchaseButton price={50} style="secondary" productSku="hardcore-mode-seven-day-access">
