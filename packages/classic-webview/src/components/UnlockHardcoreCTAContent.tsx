830a3ec1ad2777cf4e192716d89d29f371455dca
Thomas Wang <44534939+cytommi@users.noreply.github.com>
2025-05-02 15:48:17 -0400

    Fix hardcore purchase button responsive design (#71)

--- packages/classic-webview/src/components/UnlockHardcoreCTAContent.tsx
+++ packages/classic-webview/src/components/UnlockHardcoreCTAContent.tsx
@@ -13,14 +13,28 @@ export const UnlockHardcoreCTAContent = ({
   return (
     <div className="flex w-full max-w-md flex-col items-center justify-center gap-4 p-6 sm:gap-6 sm:p-8 md:max-w-2xl md:p-10">
       {withLogo && <HardcoreLogo />}
-      <p className="text-xl font-bold text-white sm:text-2xl">100 guesses. No hints. No mercy.</p>
-      <p className="text-sm font-normal text-gray-300">Get access to expert-level puzzles</p>
+      <p className="text-center text-xl font-bold text-white sm:text-2xl">
+        100 guesses. No hints. No mercy.
+      </p>
+      <p className="text-center text-sm font-normal text-gray-300">
+        Unlocking Hardcore grants access to today and all previous hardcore puzzles.
+      </p>
       <hr className="h-px w-1/2 max-w-xs bg-white/20"></hr>
-      <div className="flex w-full flex-col items-center gap-4 py-2 sm:w-auto sm:flex-row sm:items-start">
-        <PurchaseButton price={50} style="secondary" productSku="hardcore-mode-seven-day-access">
+      <div className="flex w-full flex-col items-center gap-4 py-2 sm:flex-row sm:flex-wrap sm:justify-center">
+        <PurchaseButton
+          price={50}
+          style="secondary"
+          productSku="hardcore-mode-seven-day-access"
+          shortText="7 Days"
+        >
           Unlock for 7 days
         </PurchaseButton>
-        <PurchaseButton price={250} style="primary" productSku="hardcore-mode-lifetime-access">
+        <PurchaseButton
+          price={250}
+          style="primary"
+          productSku="hardcore-mode-lifetime-access"
+          shortText="FOREVER"
+        >
           Unlock FOREVER
         </PurchaseButton>
       </div>
